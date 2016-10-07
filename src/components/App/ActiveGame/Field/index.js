1dbdd7e660749e832458d987f809deca06bfd160
Maksim <mqklin@gmail.com>
2016-10-07 22:20:49 +0500

    players can make turns

--- src/components/App/ActiveGame/Field/index.js
+++ src/components/App/ActiveGame/Field/index.js
@@ -1,14 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 import styles from './styles.scss';
 import classNames from 'classnames';
+import update from 'react-addons-update';
 
 type Props = {
   N: number,
-  onGameEnd: () => void,
+  onGameEnd: (winner: 'first' | 'second') => void,
 };
 
 type State = {
   field: ['' | 'x' | 'o'],
+  activePlayer: 'first' | 'second',
 };
 
 class Field extends Component {
@@ -17,31 +19,48 @@ class Field extends Component {
   constructor(props: Props) {
     super(props);
     this.state = {
-      field: Array(props.N * props.N).fill('\u00A0'),
+      field: Array(props.N * props.N).fill(''),
+      activePlayer: 'first',
     };
   }
+
+  handleCellClick = (idx: number): void => {
+    const { field, activePlayer } = this.state;
+    if (field[idx] !== '') return;
+    this.setState({
+      field: update(field, { [idx]: { $set: activePlayer === 'first' ? 'x' : 'o' } }) ,
+      activePlayer: activePlayer === 'first' ? 'second' : 'first',
+    });
+  };
+
   render() {
+    const { field, activePlayer } = this.state;
     const { N } = this.props;
     return (
-      <div
-        className={styles.root}
-        style={{
-          width: `${N * 50}px`,
-          height: `${N * 50}px`,
-        }}
-      >
-        {this.state.field.map((v, idx) => (
-          <span
-            key={idx}
-            className={classNames(
-              styles.cell,
-              idx % N === 0 ? styles.firstColumn : '' ,
-              idx < N ? styles.firstRow : '',
-            )}
+      <div className={styles.root}>
+        <div className={styles.header}>Ход игрока №{activePlayer === 'first' ? 1 : 2}</div>
+        <div
+          className={styles.field}
+          style={{
+            width: `${N * 50}px`,
+            height: `${N * 50}px`,
+          }}
           >
-            {v}
-          </span>
-        ))}
+          {field.map((v, idx) => (
+            <span
+              key={idx}
+              className={classNames(
+                styles.cell,
+                field[idx] !== '' ? styles.dirty : '',
+                idx % N === 0 ? styles.firstColumn : '' ,
+                idx < N ? styles.firstRow : '',
+              )}
+              onClick={() => this.handleCellClick(idx)}
+              >
+              {v || '\u00A0'}
+              </span>
+            ))}
+            </div>
       </div>
     );
   }
--- src/components/App/ActiveGame/index.js
+++ src/components/App/ActiveGame/index.js
@@ -9,43 +9,49 @@ type Props = {
   },
   onGameClose: () => void,
 };
+type State = {
+  winner: 'none' | 'first' | 'second',
+};
 
 class ActiveGame extends Component {
   props: Props;
+  state: State;
+  state = {
+    winner: 'none',
+  };
 
   render() {
     const { game } = this.props;
+    const { winner } = this.state;
     return (
       <div className={styles.root}>
-        <div className={styles.header}>
-          Ход игрока №1
+        <div className={styles.field}>
+          <Field
+            N={game.N}
+            onGameEnd={(winner: 'first' | 'second') => this.setState({ winner })}
+          />
         </div>
-        <Indent top={1}>
-          <div className={styles.field}>
-            <Field
-              N={game.N}
-            />
-          </div>
-        </Indent>
-        {/* <Indent top={1}>
-          <div className={styles.result}>
-            Победил игрок №
-          </div>
-        </Indent>
-        <Indent top={1}>
-          <div className={styles.close}>
-            <Button
-              text="Закрыть"
-              type={{
-                value: 'link',
-                options: {
-                  colorScheme: 'default',
-                },
-              }}
-              onClick={this.props.onGameClose}
-            />
-          </div>
-        </Indent> */}
+        {winner !== 'none' && [
+          <Indent top={1} key="header">
+            <div className={styles.result}>
+              Победил игрок №{}
+            </div>
+          </Indent>,
+          <Indent top={1} key="close">
+            <div className={styles.close}>
+              <Button
+                text="Закрыть"
+                type={{
+                  value: 'link',
+                  options: {
+                    colorScheme: 'default',
+                  },
+                }}
+                onClick={this.props.onGameClose}
+              />
+            </div>
+          </Indent>
+        ]}
       </div>
     );
   }
