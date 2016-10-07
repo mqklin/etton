22575312d6307699858481ec9e8ae3dc1106ede8
Maksim <mqklin@gmail.com>
2016-10-07 19:47:02 +0500

    draw field

--- src/components/App/ActiveGame/Field/index.js
+++ src/components/App/ActiveGame/Field/index.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 import styles from './styles.scss';
+import classNames from 'classnames';
 
 type Props = {
   N: number,
@@ -11,22 +12,32 @@ type State = {
 };
 
 class Field extends Component {
-  static defaultProps = {};
+  props: Props;
   state: State;
   constructor(props: Props) {
     super(props);
     this.state = {
-      field: Array(props.N).fill('\u00A0'),
+      field: Array(props.N * props.N).fill('\u00A0'),
     };
   }
   render() {
-    const { field } = this.state;
+    const { N } = this.props;
     return (
-      <div className={styles.root}>
-        {field.map((v, idx) => (
+      <div
+        className={styles.root}
+        style={{
+          width: `${N * 50}px`,
+          height: `${N * 50}px`,
+        }}
+      >
+        {this.state.field.map((v, idx) => (
           <span
             key={idx}
-            className={styles.cell}
+            className={classNames(
+              styles.cell,
+              idx % N === 0 ? styles.firstColumn : '' ,
+              idx < N ? styles.firstRow : '',
+            )}
           >
             {v}
           </span>
